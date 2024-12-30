a0da8b2a160f015f8cb4308dfd069a376f963dfd
Vladimir Kondrahin <kondervova2009@gmail.com>
2024-12-30 11:57:30 +0300

    add hometask.txt

---
+++ history.sh
@@ -0,0 +1,32 @@
+mkdir git_lab1_lesson2
+cd git_lab1_lesson2
+git clone https://github.com/YandexLyceum/human.git
+cat human.txt
+cat "human.txt"
+cat human.txt
+more human.txt
+cd human
+more human.txt
+cd C:\Users\konde\git_lab1_lesson2
+git branch
+cd human
+git branch
+git branch -a
+git fetch github
+git branch -a
+git checkout -b boots_buttons
+git fetch github
+git branch -a
+git merge boots
+git merge origin/boots
+git merge origin/buttons
+git commit -a -m 'Adr'
+git branch -a
+git merge origin/buttons
+git merge origin/boots
+git commit -a -m 'Add footer'
+git commit -a -m 'Add fo'
+git commit -m 'Add fo'
+git fetch github
+git checkout master
+doskey /history > history.sh
